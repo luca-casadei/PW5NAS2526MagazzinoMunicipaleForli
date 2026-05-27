90551616ee9f233df6c9365507bcf2dd29b91faa
TheBarbox <andrea.barbozzi@gmail.com>
2026-05-27 10:39:16 +0200

    aggiornamenti brevi

--- Frontend/src/components/global/filters.php
+++ Frontend/src/components/global/filters.php
@@ -15,19 +15,19 @@
         </div>
 
         <div class="filter-bar-group">
-            <label for="filtro-tipologia">Filtro per Tipologia</label>
+            <label for="filtro-tipologia" title="filtro ceh raoppresenart">Filtro per Tipologia</label>
             <input type="text" id="filtro-tipologia" list="lista-tipologie" class="filter-bar-input" placeholder="Scrivi o seleziona...">
             <datalist id="lista-tipologie"></datalist>
         </div>
 
         <div class="filter-bar-group">
-            <label for="filtro-attr-nome">Filtro per Attributo</label>
+            <label for="filtro-attr-nome">Filtro per Caratteristiche</label>
             <input type="text" id="filtro-attr-nome" list="lista-attributi" class="filter-bar-input" placeholder="Scrivi o seleziona...">
             <datalist id="lista-attributi"></datalist>
         </div>
 
         <div class="filter-bar-group" id="filter-bar-valore-container" style="display: none;">
-            <label for="filtro-attr-valore">Filtro per Valore Attributo</label>
+            <label for="filtro-attr-valore">Filtro per Valore Caratteristica</label>
             <input type="text" id="filtro-attr-valore" list="lista-valori" class="filter-bar-input" placeholder="Scrivi o seleziona...">
             <datalist id="lista-valori"></datalist>
         </div>
